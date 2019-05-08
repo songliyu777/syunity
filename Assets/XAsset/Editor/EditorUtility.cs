042288934d7feb7c3c75d92494c34fc84095d202
songliyu <173621801@qq.com>
2019-05-08 17:39:59 +0800

    base

---
+++ Assets/XAsset/Editor/EditorUtility.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace XAsset.Editor
+{
+    public class EditorUtility : Utility
+    { 
+        [InitializeOnLoadMethod]
+        static void Init()
+        { 
+			Debug.Log("Init->activeBundleMode: " + ActiveBundleMode);
+        } 
+
+    }
+}
