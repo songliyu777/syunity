e0a76a9905c6c95be8695e38d5f600a6a6b20567
songliyu <173621801@qq.com>
2019-05-29 20:49:35 +0800

    update

---
+++ Assets/Scripts/SYUNITY/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+using XLua;
+
+namespace SYUNITY
+{
+    [LuaCallCSharp]
+    public class UIManager : LuaMonoBehaviour
+    {
+        public void AddPackage(string packageName)
+        {
+            UIPackage.AddPackage(packageName, LoadResource);
+        }
+
+        public object LoadResource(string name, string extension, System.Type type, out DestroyMethod destroyMethod)
+        {
+            destroyMethod = DestroyMethod.None;
+            Debug.LogWarning(name + ":" + extension + ":" + type);
+            return null;
+        }
+
+        public static class UIManagerConfig
+        {
+            [CSharpCallLua]
+            public static List<Type> CSharpCallLua = new List<Type>()
+            {
+                typeof(UIManager)
+            };
+        }
+    }
+}
